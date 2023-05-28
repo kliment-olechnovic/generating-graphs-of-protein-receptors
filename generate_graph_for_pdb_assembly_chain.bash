12c0ba42eead5ac548675dc2be100e9285d1d3e1
kliment-olechnovic <kliment.olechnovic@gmail.com>
2023-05-28 17:07:37 +0300

    Redesigned tools.

--- generate_graph_for_pdb_assembly_chain.bash
+++ generate_graph_for_pdb_assembly_chain.bash
@@ -3,24 +3,23 @@
 PDBID="$(echo $1 | tr '[:upper:]' '[:lower:]')"
 ASSEMBLYNUM="$2"
 CHAINID="$3"
-FORCEFLAG="$4"
-EXTRAARG="$5"
+EXTRAARG="$4"
 
 if [ -z "$PDBID" ] || [ -z "$ASSEMBLYNUM" ] || [ -z "$CHAINID" ] || [ -n "$EXTRAARG" ]
 then
-	echo >&2 "Error: invalid arguments, need exactly three (PDBID ASSEMBLYNUM CHAINID) or four (PDBID ASSEMBLYNUM CHAINID force)"
-	exit 1
-fi
-
-if [ -n "$FORCEFLAG" ] && [ "$FORCEFLAG" != "force" ]
-then
-	echo >&2 "Error: invalid flag argument, bust be either empty or 'force'"
+	echo >&2 "Error: invalid arguments, need exactly three: PDBID ASSEMBLYNUM CHAINID"
 	exit 1
 fi
 
 cd "$(dirname $0)"
 
-./tools/download_and_preprocess_complex.bash "$PDBID" "$ASSEMBLYNUM" "$FORCEFLAG"
+COMPLEXFILE="./data/complexes/${PDBID}_as_${ASSEMBLYNUM}.pdb"
 
-./tools/extract_and_describe_monomer.bash "./data/complexes/${PDBID}_as_${ASSEMBLYNUM}.pdb" "$CHAINID" "$FORCEFLAG"
+if [ ! -s "$COMPLEXFILE" ]
+then
+	mkdir -p "$(dirname ${COMPLEXFILE})"
+	voronota-js-pdb-utensil-download-structure --id "$PDBID" --assembly "$ASSEMBLYNUM" > "$COMPLEXFILE"
+fi
+
+./tools/extract-and-describe-receptor-protein --input-complex "$COMPLEXFILE" --chain-id "$CHAINID" --output-dir './data/receptors'
 
